dfc45bee290d8966b71d75db2279a7fdc5939046
ceyhunataykan <ceyhunataykan@gmail.com>
2020-12-03 18:23:52 +0300

    Add startup page

---
+++ CA.ProductCoreApp.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CA.ProductCoreApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Demo()
+        {
+            return Ok("Asp.Net Core 3.1 Web Api JWT Auth NLayer Simple App");
+        }
+    }
+}
--- CA.ProductCoreApp.WebApi/Startup.cs
+++ CA.ProductCoreApp.WebApi/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Text;
